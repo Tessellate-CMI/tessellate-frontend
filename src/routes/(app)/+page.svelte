3c78fbd130b044edab2b5852b99d2ed2a88cf86c
Paarth314 <paarthiyer1234@gmail.com>
2023-08-22 21:06:23 +0530

    feat(component): added template hero and one section

---
+++ src/routes/(app)/+page.svelte
@@ -0,0 +1,18 @@
+<div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+        <div class="max-w-md">
+            <h1 class="text-5xl font-bold">Hello there</h1>
+            <p class="py-6">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+                exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+            <button class="btn btn-primary">Get Started</button>
+        </div>
+    </div>
+</div>
+
+<div class="flex w-full justify-center">
+    <div class="flex h-96 w-5/6 justify-center bg-primary">
+        <p class="font-bold text-white">TITLE</p>
+    </div>
+</div>
