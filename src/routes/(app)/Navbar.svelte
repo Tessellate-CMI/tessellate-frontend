364dd1f3e6cd8bb4a9aa29d0edcfcb9a6c809ce9
Paarth314 <paarthiyer1234@gmail.com>
2023-08-23 00:21:57 +0530

    feat: fixed navbar and footer for mobile
    The thebar becomes a hamburger is small width device. The second footer becomes centered in that
    case.

--- src/routes/(app)/Navbar.svelte
+++ src/routes/(app)/Navbar.svelte
@@ -1,21 +1,64 @@
-<script>
+<script lang="ts">
+    let innerWidth = 0
 </script>
 
-<div class="navbar bg-base-100">
-    <div class="flex-1">
-        <a class="btn btn-ghost text-xl normal-case" href="/">Tessellate</a>
+<svelte:window bind:innerWidth />
+
+{#if innerWidth > 640}
+    <div class="navbar bg-base-100">
+        <div class="flex-1">
+            <a class="btn btn-ghost text-xl normal-case" href="/">Tessellate</a>
+        </div>
+        <div class="gap flex-none">
+            <button class="btn btn-ghost"><a href="/events">Events</a></button>
+            <button class="btn btn-ghost"><a href="/stems/">Stems</a></button>
+            <button class="btn btn-ghost"><a href="/sponsors">Sponsors</a></button>
+            <button class="btn btn-ghost"><a href="/about-us">About Us</a></button>
+        </div>
+        <a href="/profile">
+            <span tabindex="-1" class="avatar btn btn-circle btn-ghost">
+                <div class="w-10 rounded-full">
+                    <img src="/stock_profile.jpg" alt="Profile" />
+                </div>
+            </span>
+        </a>
     </div>
-    <div class="flex-none gap-2">
-        <button class="btn btn-ghost"><a href="/events">Events</a></button>
-        <button class="btn btn-ghost"><a href="/stems/">Stems</a></button>
-        <button class="btn btn-ghost"><a href="/sponsors">Sponsors</a></button>
-        <button class="btn btn-ghost"><a href="/about-us">About Us</a></button>
+{:else}
+    <div class="navbar bg-base-100">
+        <div class="flex-1">
+            <a class="btn btn-ghost text-xl normal-case" href="/">Tessellate</a>
+        </div>
+        <details class="dropdown dropdown-end">
+            <summary class="btn btn-square btn-ghost">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    class="inline-block h-5 w-5 stroke-current"
+                    ><path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M4 6h16M4 12h16M4 18h16"
+                    ></path></svg
+                >
+            </summary>
+            <ul
+                tabindex="-1"
+                class="menu dropdown-content rounded-box menu-sm z-[1] mt-3 w-52 bg-base-100 p-1"
+            >
+                <li><button class="btn btn-ghost"><a href="/events">Events</a></button></li>
+                <li><button class="btn btn-ghost"><a href="/stems/">Stems</a></button></li>
+                <li><button class="btn btn-ghost"><a href="/sponsors">Sponsors</a></button></li>
+                <li><button class="btn btn-ghost"><a href="/about-us">About Us</a></button></li>
+            </ul>
+        </details>
+        <a href="/profile">
+            <span tabindex="-1" class="avatar btn btn-circle btn-ghost">
+                <div class="w-10 rounded-full">
+                    <img src="/stock_profile.jpg" alt="Profile" />
+                </div>
+            </span>
+        </a>
     </div>
-    <a href="/profile">
-        <span tabindex="-1" class="avatar btn btn-circle btn-ghost">
-            <div class="w-10 rounded-full">
-                <img src="/stock_profile.jpg" alt="Profile" />
-            </div>
-        </span>
-    </a>
-</div>
+{/if}
