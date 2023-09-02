dd67f4f83bbfb66b498c26435e4a0a58d50b4830
siddhantdev <agarwalsiddhant53@gmail.com>
2023-09-02 14:45:23 +0530

    refactor(component): changed navbar to new style
    Changed the navbar to look like the new style decided upon.

--- src/routes/(app)/Navbar.svelte
+++ src/routes/(app)/Navbar.svelte
@@ -4,17 +4,17 @@
 
 <svelte:window bind:innerWidth />
 
-<div class="navbar bg-base-100">
+<div class="navbar bg-main-blue font-sans text-black">
     <div class="flex-1">
-        <a class="btn btn-ghost text-xl normal-case" href="/">Tessellate</a>
+        <a class="btn btn-ghost text-2xl normal-case" href="/">Tessellate</a>
     </div>
 
     {#if innerWidth > 640}
         <div class="gap flex-none">
-            <button class="btn btn-ghost"><a href="/events">Events</a></button>
-            <button class="btn btn-ghost"><a href="/stems/">Stems</a></button>
-            <button class="btn btn-ghost"><a href="/sponsors">Sponsors</a></button>
-            <button class="btn btn-ghost"><a href="/about-us">About Us</a></button>
+            <button class="btn btn-ghost normal-case text-lg"><a href="/events">Events</a></button>
+            <button class="btn btn-ghost normal-case text-lg"><a href="/stems/">Stems</a></button>
+            <button class="btn btn-ghost normal-case text-lg"><a href="/sponsors">Sponsors</a></button>
+            <button class="btn btn-ghost normal-case text-lg"><a href="/about-us">About Us</a></button>
         </div>
     {:else}
         <details class="dropdown dropdown-end">
@@ -34,7 +34,7 @@
             </summary>
             <ul
                 tabindex="-1"
-                class="menu dropdown-content rounded-box menu-sm z-[1] mt-3 w-52 bg-base-100 p-1"
+                class="menu dropdown-content rounded-box menu-sm z-[1] mt-3 w-52 bg-base-100 p-1 gap-2"
             >
                 <li><button class="btn btn-ghost"><a href="/events">Events</a></button></li>
                 <li><button class="btn btn-ghost"><a href="/stems/">Stems</a></button></li>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import type { Config } from 'tailwindcss'
 export default {
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
-        extend: {}
+        fontFamily: {
+            display: ['Unbounded', 'display'],
+            sans: ['Space Grotesk', 'sans-serif']
+        },
+        extend: {
+            colors: {
+                'main-blue': '#c6daff'
+            }
+        }
     },
     plugins: [require('daisyui')]
 } satisfies Config
