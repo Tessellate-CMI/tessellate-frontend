fa100cfb7e29542cb13aadc4c1d4602410843f9b
Paarth314 <paarthiyer1234@gmail.com>
2023-09-05 21:07:38 +0530

    feat(footer): changed footer to fit theme
    Changed the footer to fit the theme, design provided by design team (aleem)

--- src/routes/(app)/Footer.svelte
+++ src/routes/(app)/Footer.svelte
@@ -1,59 +1,35 @@
-<footer class="footer border-t border-base-300  px-10 py-4 text-base-content bg-black">
-    <div class="grid-flow-col items-center justify-self-center sm:justify-self-start">
-        <img src="svgs/tessellate_logo_white.svg" alt="Tessellate Logo" style="width: 35px" />
-        <p style="font-size: 1.4rem">TESSELATE</p>
-    </div>
-    <div class="justify-self-center sm:justify-self-end">
-        <div class="grid grid-flow-col gap-3">
-            <a href="https://www.instagram.com/tessellate_cmi">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="25"
-                    height="25"
-                    viewBox="0 0 24 24"
-                    class="fill-current"
-                >
-                    <path
-                        d="M 8 3 C 5.243 3 3 5.243 3 8 L 3 16 C 3 18.757 5.243 21 8 21 L 16 21 C 18.757 21 21 18.757 21 16 L 21 8 C 21 5.243 18.757 3 16 3 L 8 3 z M 8 5 L 16 5 C 17.654 5 19 6.346 19 8 L 19 16 C 19 17.654 17.654 19 16 19 L 8 19 C 6.346 19 5 17.654 5 16 L 5 8 C 5 6.346 6.346 5 8 5 z M 17 6 A 1 1 0 0 0 16 7 A 1 1 0 0 0 17 8 A 1 1 0 0 0 18 7 A 1 1 0 0 0 17 6 z M 12 7 C 9.243 7 7 9.243 7 12 C 7 14.757 9.243 17 12 17 C 14.757 17 17 14.757 17 12 C 17 9.243 14.757 7 12 7 z M 12 9 C 13.654 9 15 10.346 15 12 C 15 13.654 13.654 15 12 15 C 10.346 15 9 13.654 9 12 C 9 10.346 10.346 9 12 9 z"
-                    ></path>
-                </svg>
-            </a>
-            <a href="https://twitter.com/cmitessellate"
-                ><svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    class="fill-current"
-                    ><path
-                        d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
-                    ></path></svg
-                ></a
-            >
-            <a href="https://www.youtube.com/TessellateCMI"
-                ><svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    class="fill-current"
-                    ><path
-                        d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"
-                    ></path></svg
-                ></a
-            >
-            <a href="https://www.facebook.com/CMITessellate"
-                ><svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    class="fill-current"
-                    ><path
-                        d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"
-                    ></path></svg
-                ></a
-            >
-        </div>
-    </div>
+<footer class="footer items-center bg-black px-4 py-10 text-white">
+    <aside class="grid-flow-col items-center">
+        <img src="svgs/tessellate_logo_white.svg" alt="Tessellate Logo" style="width: 40px" />
+        <p class="font-display text-xl">TESSELLATE</p>
+    </aside>
+    <nav class="flex flex-col sm:flex-row sm:gap-8 md:place-self-center md:justify-self-end">
+        <a
+            href="https://www.instagram.com/tessellate_cmi"
+            class="text-xl font-bold hover:underline hover:underline-offset-2"
+        >
+            Instagram &#129125;</a
+        >
+        <a
+            href="https://www.facebook.com/CMITessellate"
+            class="text-xl font-bold hover:underline hover:underline-offset-2"
+        >
+            Facebook &#129125;</a
+        >
+        <a
+            href="https://twitter.com/cmitessellate"
+            class="text-xl font-bold hover:underline hover:underline-offset-2"
+        >
+            Twitter &#129125;</a
+        >
+        <a
+            href="https://www.youtube.com/TessellateCMI"
+            class="text-xl font-bold hover:underline hover:underline-offset-2"
+        >
+            YouTube &#129125;</a
+        >
+    </nav>
+    <!-- <a href="." class="text-slate-400"
+        >Subscribe to the Tessellate newsletter to keep up with updates</a
+    > -->
 </footer>
