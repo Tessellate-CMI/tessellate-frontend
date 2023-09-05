c81350d479f9eb583f2bbfed709c0c577a5d350f
Paarth314 <paarthiyer1234@gmail.com>
2023-09-05 21:12:57 +0530

    style: prettier formatting
    Prettier removed a comma, pushing for consistency

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ export default {
         extend: {
             colors: {
                 'main-blue': '#c6daff',
-                'second-blue': '#1e5cc4',
+                'second-blue': '#1e5cc4'
             }
         }
     },
