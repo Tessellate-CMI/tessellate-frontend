9e04b38ae011e81fde1c0e27253e696d27c94ac4
Paarth314 <paarthiyer1234@gmail.com>
2023-09-16 18:42:03 +0530

    fix: fixed weird url error

--- src/routes/(app)/Footer.svelte
+++ src/routes/(app)/Footer.svelte
@@ -1,6 +1,6 @@
 <footer class="footer items-center bg-black px-4 py-10 text-white">
     <aside class="grid-flow-col items-center">
-        <img src="svgs/tessellate_logo_white.svg" alt="Tessellate Logo" style="width: 40px" />
+        <img src="/svgs/tessellate_logo_white.svg" alt="Tessellate Logo" style="width: 40px" />
         <p class="font-display text-xl">TESSELLATE</p>
     </aside>
     <nav class="flex flex-col sm:flex-row sm:gap-8 md:place-self-center md:justify-self-end">
