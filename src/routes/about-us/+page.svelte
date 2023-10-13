e06a7ce07b52c90e9a04bbc78f5d2bdfe9008c15
Paarth314 <paarthiyer1234@gmail.com>
2023-10-13 14:33:25 +0530

    feat: added footer to about us page

--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -2,21 +2,22 @@
     import '$src/app.css'
     import Navbar from '$src/lib/components/Navbar.svelte'
     import team from '$lib/team.json'
+    import Footer from '$src/lib/components/Footer.svelte'
 </script>
 
 <Navbar absolute={false} bgColor="bg-second-blue" textWhite={true} />
 
 <div
-    class="grid w-full grid-cols-1 border-b-2 border-white bg-second-blue px-5 lg:px-10
-            pb-10 text-white lg:grid-cols-3 sm:pb-12 sm:pt-24"
+    class="grid w-full grid-cols-1 border-b-2 border-white bg-second-blue px-5 pb-10
+            text-white sm:pb-12 sm:pt-24 lg:grid-cols-3 lg:px-10"
 >
     <h1
-        class="lg:col-span-1 h-full w-full border-white pb-2 pt-10 font-heading text-6xl font-semibold sm:pb-12 sm:text-8xl"
+        class="h-full w-full border-white pb-2 pt-10 font-heading text-6xl font-semibold sm:pb-12 sm:text-8xl lg:col-span-1"
     >
         About CMI
     </h1>
     <p
-        class="lg:col-span-2 max-w-screen-lg pt-5 text-xl font-medium lg:border-l-2 sm:pb-12 lg:pl-16 sm:pr-8 sm:text-2xl"
+        class="max-w-screen-lg pt-5 text-xl font-medium sm:pb-12 sm:pr-8 sm:text-2xl lg:col-span-2 lg:border-l-2 lg:pl-16"
     >
         Chennai Mathematical Institute (CMI) is a renowned institution for mathematics and computer
         science education and research, founded in 1989. It became autonomous in 1996 and is known
@@ -27,7 +28,7 @@
     </p>
 </div>
 
-<div class="w-full bg-main-blue pl-5 md:pl-10 text-black">
+<div class="w-full bg-main-blue pl-5 text-black md:pl-10">
     <h1 class="pb-2 pt-16 font-heading text-6xl font-semibold sm:pb-8 sm:text-8xl">
         Meet the Tess'24 Team
     </h1>
@@ -45,14 +46,15 @@
                     {#each teamName.members as member}
                         <div>
                             <div
-                                class="m-3 md:m-7 flex max-w-[300px] flex-col border-l-2 border-black p-2 px-3 md:px-5 text-xl"
+                                class="m-3 flex max-w-[300px] flex-col border-l-2 border-black p-2 px-3 text-xl md:m-7 md:px-5"
                             >
                                 <div class="pb-2 text-2xl font-semibold sm:text-3xl">
                                     {member.name}
                                 </div>
                                 <div>{member.batch}</div>
-                                <a href="mailto:{member.email}" class="text-lg sm:text-xl hover:text-blue-600"
-                                    >{member.email}</a
+                                <a
+                                    href="mailto:{member.email}"
+                                    class="text-lg hover:text-blue-600 sm:text-xl">{member.email}</a
                                 >
                             </div>
                         </div>
@@ -62,3 +64,5 @@
         {/each}
     </div>
 </div>
+
+<Footer />
