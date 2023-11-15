991c8d0a722afc516b4ded98f6a3cdb1e0a94d9a
Paarth314 <paarthiyer1234@gmail.com>
2023-11-15 10:49:41 +0530

    feat(structure): restructured the folders and added the event subfolders

--- src/routes/competitions/+page.svelte
+++ src/routes/competitions/+page.svelte
@@ -1,6 +1,20 @@
 <script>
     import '$src/app.css'
-    import ComingSoon from '$lib/components/ComingSoon.svelte'
+    import Event from '$src/lib/components/Event.svelte'
+    import Navbar from '$src/lib/components/Navbar.svelte'
+    import Footer from '$src/lib/components/Footer.svelte'
 </script>
 
-<ComingSoon />
+<Navbar absolute={false}/>
+
+<div class="bg-white text-black">
+    <Event name="Chess">
+        Chess is an awesome sport!
+    </Event>
+
+    <Event name="Quiz" colorOpt={2}>
+        Quizzes are fun.
+    </Event>
+</div>
+
+<Footer />
--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,20 +1,6 @@
 <script>
     import '$src/app.css'
-    import Event from '$src/lib/components/Event.svelte'
-    import Navbar from '$src/lib/components/Navbar.svelte'
-    import Footer from '$src/lib/components/Footer.svelte'
+    import ComingSoon from '$lib/components/ComingSoon.svelte'
 </script>
 
-<Navbar absolute={false}/>
-
-<div class="bg-white text-black">
-    <Event name="Chess">
-        Chess is an awesome sport!
-    </Event>
-
-    <Event name="Quiz" colorOpt={2}>
-        Quizzes are fun.
-    </Event>
-</div>
-
-<Footer />
+<ComingSoon />
