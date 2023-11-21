fbc4273039a0ad84412a8fb3548643d5fcd77542
Paarth314 <paarthiyer1234@gmail.com>
2023-11-21 19:36:14 +0530

    feat(event page): minor formatting changes, swapped desc prop with a slot for better control

--- src/lib/components/Event.svelte
+++ src/lib/components/Event.svelte
@@ -3,17 +3,8 @@
     import type { EventPartners_large } from '$lib/components/interfaces'
 
     export let name = 'Event name'
-    export let desc = [
-        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
-    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
-    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
-    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id 
-    est laborum.`
-    ]
     export let register = 'register'
-    export let rules_pdf = '/'
+    export let rules_pdf = ''
     export let image = '/events/default.jpg'
     export let key_rules = ['First rule', 'Second rule', 'Third rule']
     export let event_partners: EventPartners_large[] = []
@@ -25,39 +16,38 @@
     }
 </script>
 
-<div class="w-full bg-main-blue pb-4">
-    <!-- <div
-        style="background-image: linear-gradient(180deg, #fff0,  #ffff)"
-        class="mx-auto h-10 w-full max-w-screen-lg"
-    ></div> -->
+<div class="w-full bg-main-blue pb-4 text-black">
     <div
         class="z-10 mx-auto w-full max-w-screen-lg bg-white pl-8 pt-10
                font-heading text-[11.5vw] font-bold leading-none text-sky-950 sm:pb-5 md:text-8xl"
     >
         {name}
     </div>
-
-    <!-- <div class="mx-auto w-full max-w-screen-xl border-t-4 border-second-blue"></div> -->
-    <!-- <div class="mx-auto h-[6vw] w-full max-w-screen-lg bg-white sm:h-8"></div> -->
-
     <div class="relative mx-auto flex w-full max-w-screen-xl pb-10">
         <div class="absolute z-0 h-full w-full">
             <div class="mx-auto h-full w-full max-w-screen-lg bg-white"></div>
         </div>
         <div class="z-10 grid w-full grid-cols-1 items-center object-right pt-5 md:grid-cols-2">
             <div class="flex md:w-full md:justify-end">
-                <div class="px-8 text-lg font-medium sm:text-xl md:-mr-20 md:w-[592px]">
-                    {#each desc as para}
-                        <p class="text-justify">
-                            {para}
-                        </p>
-                    {/each}
-                    <a
-                        href={register}
-                        style="text-transform:none; font-size:1.5rem"
-                        class="btn btn-outline m-auto mt-5 h-auto min-h-0 w-full rounded-none border-4 py-3 text-sky-950 hover:border-sky-950 hover:bg-sky-950 hover:text-white sm:text-xl"
-                        >Register</a
-                    >
+                <div
+                    class="px-8 text-justify text-lg font-medium sm:text-xl md:-mr-20 md:w-[592px]"
+                >
+                    <slot />
+                    {#if register != ''}
+                        <a
+                            href={register}
+                            style="text-transform:none; font-size:1.5rem"
+                            class="btn btn-outline m-auto mt-5 h-auto min-h-0 w-full rounded-none border-4 py-3 text-sky-950 hover:border-sky-950 hover:bg-sky-950 hover:text-white sm:text-xl"
+                            >Register</a
+                        >
+                    {:else}
+                        <div
+                            style="text-transform:none; font-size:1.5rem"
+                            class="btn btn-outline m-auto mt-5 h-auto min-h-0 w-full rounded-none border-4 py-3 text-sky-950 hover:border-sky-950 hover:bg-sky-950 hover:text-white sm:text-xl"
+                        >
+                            Registrations starting soon!
+                        </div>
+                    {/if}
                     <div class="mt-3 text-sm text-red-700 sm:text-base">
                         Please go through the event details and rules listed below before you
                         register, as to avoid any confusions.
@@ -83,23 +73,25 @@
         </div>
         <div class="mr-8 mt-5 text-lg font-medium sm:text-xl">
             <div class="border-sky-950 pl-8 pt-6 md:ml-8 md:border-l-[3px] md:pl-8 md:pt-0">
-                <p class="my-3 text-xl">
-                    Registration Cost <br /><span class="text-2xl font-semibold"
+                <p class="my-3 text-lg sm:text-xl">
+                    Registration Cost <br /><span class="text-xl font-semibold sm:text-2xl"
                         >{details.cost}</span
                     >
                 </p>
-                <p class="my-3 text-xl">
-                    Last date to register <br /><span class="text-2xl font-semibold"
+                <p class="my-3 text-lg sm:text-xl">
+                    Last date to register <br /><span class="text-xl font-semibold sm:text-2xl"
                         >{details.deadline}</span
                     >
                 </p>
-                <p class="my-3 text-xl">
-                    When is it happening <br /><span class="text-2xl font-semibold"
+                <p class="my-3 text-lg sm:text-xl">
+                    When is it happening <br /><span class="text-xl font-semibold sm:text-2xl"
                         >{details.date}</span
                     >
                 </p>
-                <p class="my-3 text-xl">
-                    Event venue <br /><span class="text-2xl font-semibold">{details.venue}</span>
+                <p class="my-3 text-lg sm:text-xl">
+                    Event venue <br /><span class="text-xl font-semibold sm:text-2xl"
+                        >{details.venue}</span
+                    >
                 </p>
             </div>
         </div>
@@ -141,12 +133,21 @@
             >
                 Rules & <br />Regulations
             </div>
-            <a
-                href={rules_pdf}
-                style="text-transform:none"
-                class="btn btn-outline m-auto ml-8 mt-10 h-auto min-h-0 rounded-none px-5 py-3 text-lg text-sky-950 hover:bg-sky-950 hover:text-white sm:px-10 sm:text-xl"
-                >Complete list of rules</a
-            >
+            {#if rules_pdf != ''}
+                <a
+                    href={rules_pdf}
+                    style="text-transform:none"
+                    class="btn btn-outline m-auto ml-8 mt-10 h-auto min-h-0 rounded-none px-5 py-3 text-lg text-sky-950 hover:bg-sky-950 hover:text-white sm:px-10 sm:text-xl"
+                    >Complete list of rules</a
+                >
+            {:else}
+                <div
+                    style="text-transform:none"
+                    class="btn btn-outline m-auto ml-8 mt-10 h-auto min-h-0 rounded-none px-5 py-3 text-lg text-sky-950 hover:bg-sky-950 hover:text-white sm:px-10 sm:text-xl"
+                >
+                    Complete list of rules coming soon!
+                </div>
+            {/if}
         </div>
         <div class="mr-8 mt-5 text-lg font-medium sm:text-xl">
             <ol class="list-decimal pl-16 pt-6 md:pl-0 md:pt-0">
