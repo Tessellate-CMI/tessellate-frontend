66ba6325c0b48d2f10d71e0e1ed0b580d3c5d467
Paarth314 <paarthiyer1234@gmail.com>
2023-11-21 19:40:57 +0530

    feat(events): modified existing pages to fit the new formant of the event component

--- src/routes/competitions/debate-individual/+page.svelte
+++ src/routes/competitions/debate-individual/+page.svelte
@@ -4,37 +4,43 @@
     import Navbar from '$src/lib/components/Navbar.svelte'
     import Footer from '$src/lib/components/Footer.svelte'
 
-    const desc = [
-        `A snappy one-on-one debate where your arguments must keep pace with the unpredictable bell chime. 
-        Can you effectively convey yourself before time runs out?`,
-        `This year, we are pleased to say our debate events will be judged by a panel from A to Zee creativity.`
-    ]
-
     const key_rules: string[] = []
 
     const details = {
         cost: 'INR 200 per person',
         deadline: 'TBD',
-        date: 'TBD',
-        venue: 'TBD'
+        date: '27th January, 2024',
+        venue: 'LH5, LH6 and Auditorium (CMI)'
     }
 
-    export let rules_pdf = '/'
-    export let image = '/events/default.jpg'
-    export let event_partners = [
-        {
-            name: 'A to Zee creativity',
-            image: 'AtoZeeCreativity.webp',
-            desc: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
-        }
-    ]
+    const rules_pdf = ''
+    const image = '/events/default.jpg'
+    const register = ''
+    // const event_partners = [
+    //     {
+    //         name: 'A to Zee creativity',
+    //         image: 'AtoZeeCreativity.webp',
+    //         desc: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+    //     }
+    // ]
 </script>
 
 <Navbar absolute={false} />
 
 <div class="bg-white text-black">
-    <Event name="Turncoat Debate" {desc} {key_rules} {details} {rules_pdf} {image} {event_partners}
-    ></Event>
+    <Event name="Battle the Time" {key_rules} {details} {rules_pdf} {image} {register}>
+        <p>
+            Where debonair debaters duel with deft discourse, delivering delightful dialogues in a
+            riveting rhythmic rendition of rhetoric.
+        </p>
+        <p class="mt-3 text-2xl font-semibold text-sky-950">Battle the Time</p>
+        <p>
+            A snappy one-on-one debate where your arguments must keep pace with the unpredictable
+            bell chime. Can you effectively convey yourself before time runs out? <br />
+            This year, we are pleased to say our debate events will be judged by a panel from A to Zee
+            creativity.
+        </p>
+    </Event>
 </div>
 
 <Footer />
--- src/routes/competitions/quiz/+page.svelte
+++ src/routes/competitions/quiz/+page.svelte
@@ -4,31 +4,31 @@
     import Navbar from '$src/lib/components/Navbar.svelte'
     import Footer from '$src/lib/components/Footer.svelte'
 
-    const desc = [
-        `Every year, our quiz show event captivates participants with intriguing questions spanning a kaleidoscope of categories. 
-        From the annals of history to the intricacies of pop culture; from scientific realms to literary landscapes; 
-        prepare to put your knowledge to the test, as every obscure fact you've come across will be called upon.`,
-        `This year, we are honoured to have eminent quizzer Major Chandrakant Nair as our Quizmaster. 
-        Come join us for an exciting day of cerebral combat!`
-    ]
-
     const key_rules: string[] = []
 
     const details = {
         cost: 'INR 200 per person',
         deadline: 'TBD',
-        date: 'TBD',
-        venue: 'TBD'
+        date: ' 28th January, 2024 ',
+        venue: ' Auditorium, CMI'
     }
 
-    export let rules_pdf = '/'
-    export let image = '/events/default.jpg'
+    const rules_pdf = ''
+    const image = '/events/quiz.webp'
+    const register = ''
 </script>
 
 <Navbar absolute={false} />
 
-<div class="bg-white text-black">
-    <Event name="Quiz" {desc} {key_rules} {details} {rules_pdf} {image}></Event>
-</div>
+<Event name="Quiz" {key_rules} {details} {rules_pdf} {image} {register}>
+    <p>
+        Every year, our quiz show event captivates participants with intriguing questions spanning a
+        kaleidoscope of categories. From the annals of history to the intricacies of pop culture;
+        from scientific realms to literary landscapes; prepare to put your knowledge to the test, as
+        every obscure fact you've come across will be called upon. <br /> This year, we are honoured
+        to have eminent quizzer <span class="font-bold">Major Chandrakant Nair</span> as our Quizmaster.
+        Come join us for an exciting day of cerebral combat!
+    </p>
+</Event>
 
 <Footer />
