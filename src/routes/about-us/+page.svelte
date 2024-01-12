4747229acc28c28e3d5eb2fc4a59924fd1737734
siddhantdev <agarwalsiddhant53@gmail.com>
2024-01-12 11:25:26 +0530

    fix: updated about us page

--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -26,13 +26,23 @@
         significant funding, and was recognized as a university in 2006. CMI aspires to grow into a
         comprehensive academic institution akin to top universities globally.
     </p>
+    <div class="mt-6 flex">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3890.2293489089448!2d80.21755457631039!3d12.828450917998072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525a6872b56423%3A0x6bdb1e0a06c8a878!2sChennai%20Mathematical%20Institute!5e0!3m2!1sen!2sin!4v1705038201951!5m2!1sen!2sin"
+            width="600"
+            height="450"
+            style="border:0;"
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+            title="CMI Location Map"
+        ></iframe>
+    </div>
 </div>
-
 <div class="w-full bg-main-blue pl-5 text-black md:pl-10">
     <h1 class="pb-2 pt-16 font-heading text-6xl font-semibold sm:pb-8 sm:text-8xl">
         Meet the Tess'24 Team
     </h1>
-    <div class="grid grid-cols-1 gap-3 md:grid-cols-2">
+    <div class="grid grid-cols-1 gap-3">
         {#each team as teamName}<div class="flex flex-col">
                 <div class="pb-3">
                     <h1 class=" pt-10 font-heading text-4xl font-semibold sm:text-5xl">
