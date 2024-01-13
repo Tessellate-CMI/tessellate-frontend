4d9bdcb040ece9d01caad62585c5973b4a8c437a
siddhantdev <agarwalsiddhant53@gmail.com>
2024-01-13 17:26:53 +0530

    fix: updated events page

--- src/lib/components/InternalEvent.svelte
+++ src/lib/components/InternalEvent.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div class="m-5 border-2 border-black bg-white p-6 text-black">
-    <h1 class="font-sans text-3xl font-bold">{name}</h1>
+    <h1 class="font-sans text-3xl font-bold md:text-2xl lg:text-3xl">{name}</h1>
     <p class="mt-3">{description}</p>
     <p class="mt-3 text-lg font-semibold">
         Alumni Permitted: {alumni === 'true' ? 'Yes' : alumni === 'false' ? 'No' : alumni}
