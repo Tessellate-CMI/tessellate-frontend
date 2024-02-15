59f3d007273deeba0ccc0f3e0cf31b4c9563d0df
siddhantdev <agarwalsiddhant53@gmail.com>
2024-02-15 15:06:31 +0530

    feat: updated abstracts for talks

--- src/lib/components/StemsSchedule.svelte
+++ src/lib/components/StemsSchedule.svelte
@@ -13,5 +13,6 @@
 <p class="text-3xl">{title}</p>
 <p><span class="text-lg font-semibold">{time}</span></p>
 {#if abstract != ''}
-    <p class="my-2">{abstract}</p>
+    <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+    <p class="my-2">{@html abstract}</p>
 {/if}
