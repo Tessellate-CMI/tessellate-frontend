0f442dced9169c47a20f8cd34c57e6610b0e5d40
Paarth314 <paarthiyer1234@gmail.com>
2024-09-18 13:57:03 +0530

    feat(theme): added colour and (temporary) fonts
    Added these to the tailwind config as theme extensions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,22 @@ import type { Config } from 'tailwindcss'
 export default {
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
-        extend: {}
+        fontFamily: {
+            heading: ['Ubuntu', 'sans-serif'],
+            sans: ['Open Sans', 'sans-serif'],
+            body: ['Open Sans']
+        },
+        extend: {
+            colors: {
+                brick: '#f36a46',
+                pista: '#c7dba8',
+                butterscotch: '#fff0ab',
+                skin: '#fbe6d5',
+                'sea-green': '#87b6ac',
+                'off-white': '#fdfdff',
+                caramel: '#c99652'
+            }
+        }
     },
     plugins: []
 } satisfies Config
