d5552f26a5783cdf182e3a8c95717afa6fc359ee
Paarth314 <paarthiyer1234@gmail.com>
2024-11-18 02:00:26 +0530

    feat: added resources and details page

---
+++ src/routes/stems/details/+page.svelte
@@ -0,0 +1,137 @@
+<div class="mt-5 flex w-full flex-col">
+    <div class="mx-auto w-full max-w-screen-lg shadow-md">
+        <div class="w-full p-6 py-10 text-center font-heading text-6xl font-semibold lg:text-8xl">
+            Details
+        </div>
+    </div>
+    <div class="mx-auto mt-5 w-full max-w-screen-lg shadow-md">
+        <div class="grid grid-cols-1 text-lg md:grid-cols-2 lg:text-xl">
+            <div class=" border-black px-4 py-4 sm:px-8 md:border-r-2 md:pb-16 md:pt-6">
+                <div class="mb-6 w-full text-center font-heading text-5xl font-medium md:mb-12">
+                    Eligibility
+                </div>
+                <ul class="list-inside list-disc space-y-4 px-4">
+                    <li>Students of Class 8 till 12</li>
+                    <li>
+                        Students pursuing an undergraduate degree (need not be in a STEMS field)
+                    </li>
+                </ul>
+            </div>
+            <div class="px-4 py-6 sm:px-8 md:py-6">
+                <div class="mb-6 w-full text-center font-heading text-5xl font-medium">Dates</div>
+                <ul class="list-inside list-disc space-y-4 px-4">
+                    <li>TBA</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="mx-auto mb-10 mt-5 w-full max-w-screen-lg shadow-md">
+        <div class="my-10 w-full text-center font-heading text-5xl font-medium">
+            Subjects / Categories
+        </div>
+        <div class="my-16 grid grid-cols-1 text-lg md:text-xl lg:grid-cols-3">
+            <div class="border-black px-8 lg:border-r-2">
+                <div class="text-center font-heading text-3xl font-medium">Mathematics</div>
+                <p class="mt-8"><strong>Category A</strong><br />Grade 8, 9, 10</p>
+                <p class="mt-8">
+                    <strong>Category B</strong><br />Grade 11, 12, Undergrad 1st year
+                </p>
+                <p class="mt-8"><strong>Category C</strong><br />Undergrad 2nd to final year</p>
+                <div class="flex border-black px-6 lg:border-r-2">
+                    <a
+                        href="/"
+                        class=" m-auto mt-12 border-2 border-black px-6 py-2 text-xl transition-all duration-300 hover:border-gray-600 hover:bg-gray-600 hover:text-white lg:hidden"
+                        >Syllabus</a
+                    >
+                </div>
+            </div>
+            <div class="border-black px-8 lg:border-r-2">
+                <div class="mt-16 text-center font-heading text-3xl font-medium lg:mt-0">
+                    Physics
+                </div>
+                <p class="mt-8"><strong>Category A</strong><br />Grade 8, 9, 10</p>
+                <p class="mt-8">
+                    <strong>Category B</strong><br />Grade 11, 12, Undergrad 1st year
+                </p>
+                <p class="mt-8"><strong>Category C</strong><br />Undergrad 2nd to final year</p>
+                <div class="flex border-black px-6 lg:border-r-2">
+                    <a
+                        href="/"
+                        class=" m-auto mt-12 border-2 border-black px-6 py-2 text-xl transition-all duration-300 hover:border-gray-600 hover:bg-gray-600 hover:text-white lg:hidden"
+                        >Syllabus</a
+                    >
+                </div>
+            </div>
+            <div class=" px-8">
+                <div class="mt-16 text-center font-heading text-3xl font-medium lg:mt-0">
+                    Computer Science
+                </div>
+                <p class="mt-8"><strong>Category A</strong><br />Grade 8, 9, 10, 11, 12</p>
+                <p class="mt-8">
+                    <strong>Category B</strong><br />Undergrad 1st to final year
+                </p>
+                <div class="flex border-black px-6 lg:border-r-2">
+                    <a
+                        href="/"
+                        class=" m-auto mt-12 border-2 border-black px-6 py-2 text-xl transition-all duration-300 hover:border-gray-600 hover:bg-gray-600 hover:text-white lg:hidden"
+                        >Syllabus</a
+                    >
+                </div>
+            </div>
+            <div class="flex border-r-2 border-black px-6">
+                <a
+                    href="/"
+                    class=" m-auto mt-12 hidden border-2 border-black px-6 py-2 text-xl transition-all duration-300 hover:border-gray-600 hover:bg-gray-600 hover:text-white lg:block"
+                    >Syllabus</a
+                >
+            </div>
+            <div class="flex border-r-2 border-black px-6">
+                <a
+                    href="/"
+                    class="m-auto mt-12 hidden border-2 border-black px-6 py-2 text-xl transition-all duration-300 hover:border-gray-600 hover:bg-gray-600 hover:text-white lg:block"
+                    >Syllabus</a
+                >
+            </div>
+            <div class="flex px-6">
+                <a
+                    href="/"
+                    class="m-auto mt-12 hidden border-2 border-black px-6 py-2 text-xl transition-all duration-300 hover:border-gray-600 hover:bg-gray-600 hover:text-white lg:block"
+                    >Syllabus</a
+                >
+            </div>
+        </div>
+    </div>
+    <div class="mx-auto mb-10 mt-5 w-full max-w-screen-lg shadow-md">
+        <div class="my-10 w-full px-14 text-center font-heading text-5xl font-medium">
+            Rules <span class="font-body">&</span> Regulation
+        </div>
+        <div class="mb-8 mt-10 px-14 text-lg md:px-20 md:text-xl">
+            <ol class="list-outside list-disc space-y-4">
+                <li>
+                    Students are allowed to refer to books and online sources to solve the problems.
+                </li>
+
+                <li>
+                    The problems should not be uploaded on any forums or websites for discussion
+                    during exam time. Candidates will be disqualified in case of any misconduct
+                    during the exam.
+                </li>
+
+                <li>
+                    Your solutions must be strictly original. There might be an interview for
+                    confirmation after the selection is made. In case of any discrepancies, the
+                    submission of the student in question will be invalidated.
+                </li>
+
+                <li>Submissions made after the deadline will not be accepted.</li>
+            </ol>
+        </div>
+        <div class="flex px-6">
+            <a
+                href="/"
+                class="m-auto mb-12 border-2 border-black px-8 py-3 text-xl transition-all duration-300 hover:border-gray-600 hover:bg-gray-600 hover:text-white"
+                >Complete set of Rules</a
+            >
+        </div>
+    </div>
+</div>
---
+++ src/routes/stems/resources/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import PastYearStuff from '../../../lib/components/PastYearStuff.svelte'
+</script>
+
+<div class="mt-5 flex w-full flex-col">
+    <div class="mx-auto w-full max-w-screen-lg shadow-md">
+        <div class="w-full p-6 py-10 text-center font-heading text-7xl font-semibold md:text-8xl">
+            Resources
+        </div>
+    </div>
+    <PastYearStuff year={2024} physics={['A', 'B', 'C']} maths={['A', 'B', 'C']} cs={['A', 'B']} />
+    <PastYearStuff
+        year={2023}
+        physics={['A', 'B', 'C']}
+        maths={['A', 'A(2)', 'B', 'B(2)', 'C']}
+        cs={['A', 'B']}
+    />
+    <PastYearStuff year={2021} physics={['A', 'B', 'C']} maths={['A', 'B', 'C']} cs={['A', 'B']} />
+    <PastYearStuff year={2020} physics={['A', 'B']} maths={['A', 'B']} cs={['A', 'B']} />
+</div>
