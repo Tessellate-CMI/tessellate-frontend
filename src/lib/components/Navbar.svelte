a0fb99a936c8e72738cdb18c6411337edabab053
Paarth314 <paarthiyer1234@gmail.com>
2024-11-20 00:40:06 +0530

    feat: navbar trimming

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,17 +1,17 @@
-<div class="absolute inline-flex h-12 w-full items-center border-black md:h-16">
+<div class="absolute z-10 mt-2 inline-flex h-16 w-full items-center border-black md:mt-0 md:h-16">
     <div class="flex h-full flex-1">
-        <a class="flex border-black px-2 md:px-6" href="/"
+        <a class="flex border-black px-2 md:px-6" href="/stems"
             ><img src="/logos/TESSELLATE-black.svg" alt="Tessellate Logo" style="width: 50px" /></a
         >
     </div>
-    <div class="flex h-full font-heading md:text-xl">
+    <!-- <div class="flex h-full font-heading md:text-xl">
         <button
-            class="flex h-full items-center border-b-2 border-l-2 border-black bg-white bg-opacity-65 px-2 font-semibold transition-all duration-300 ease-in-out hover:bg-sea-green hover:bg-opacity-100 hover:text-white md:px-4"
+            class="bg-off flex h-full items-center border-b-2 border-l-2 border-black bg-white bg-opacity-65 px-2 font-semibold transition-all duration-300 ease-in-out hover:bg-sea-green hover:bg-opacity-100 hover:text-white md:px-4"
             ><a href="/stems">STEMS</a></button
         >
         <button
             class="flex h-full items-center border-b-2 border-l-2 border-black bg-white bg-opacity-65 px-2 font-semibold transition-all duration-300 ease-in-out hover:bg-brick hover:bg-opacity-100 hover:text-white md:px-4"
             ><a href="/">Contact Us</a></button
         >
-    </div>
+    </div> -->
 </div>
